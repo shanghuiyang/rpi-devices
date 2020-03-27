8e1bae4722a9a27d173714d4bc445b5200e85c2f
Shanghui Yang <shanghui.yang@grabtaxi.com>
2020-03-28 01:01:46 +0800

    add alert

--- app/vmonitor/vmonitor.go
+++ app/vmonitor/vmonitor.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
+	"time"
 
 	"github.com/shanghuiyang/rpi-devices/base"
 	"github.com/shanghuiyang/rpi-devices/dev"
@@ -64,8 +67,10 @@ func main() {
 
 	log.Printf("video monitor server started")
 	base.WaitQuit(func() {
+		vMonitor.stop()
 		rpio.Close()
 	})
+	vMonitor.start()
 
 	http.HandleFunc("/", handler)
 	err := http.ListenAndServe(":8080", nil)
@@ -109,33 +114,45 @@ func operationHandler(w http.ResponseWriter, r *http.Request) {
 	case "down":
 		go vMonitor.down()
 	case "beep":
-		go vMonitor.beep()
+		go vMonitor.beep(5, 100)
 	default:
 		log.Printf("invalid operator: %v", op)
 	}
 }
 
 type vmonitor struct {
-	hServo *dev.SG90
-	vServo *dev.SG90
-	led    *dev.Led
-	buzzer *dev.Buzzer
-	hAngle int
-	vAngle int
+	hServo  *dev.SG90
+	vServo  *dev.SG90
+	led     *dev.Led
+	buzzer  *dev.Buzzer
+	hAngle  int
+	vAngle  int
+	chAlert chan int
 }
 
 func newVMonitor(hServo, vServo *dev.SG90, led *dev.Led, buzzer *dev.Buzzer) *vmonitor {
 	v := &vmonitor{
-		hServo: hServo,
-		vServo: vServo,
-		led:    led,
-		buzzer: buzzer,
-		hAngle: 0,
-		vAngle: 0,
+		hServo:  hServo,
+		vServo:  vServo,
+		led:     led,
+		buzzer:  buzzer,
+		hAngle:  0,
+		vAngle:  0,
+		chAlert: make(chan int, 16),
 	}
-	v.hServo.Roll(v.hAngle)
-	v.vServo.Roll(v.vAngle)
 	return v
+}
+
+func (v *vmonitor) start() {
+	go v.hServo.Roll(v.hAngle)
+	go v.vServo.Roll(v.vAngle)
+	go v.detectConnecting()
+	go v.alert()
+}
+
+func (v *vmonitor) stop() {
+	v.led.Off()
+	close(v.chAlert)
 }
 
 func (v *vmonitor) left() {
@@ -182,10 +199,57 @@ func (v *vmonitor) down() {
 	v.vServo.Roll(angle)
 }
 
-func (v *vmonitor) beep() {
+func (v *vmonitor) beep(n int, interval int) {
 	log.Printf("op: beep")
 	if v.buzzer == nil {
 		return
 	}
-	v.buzzer.Beep(5, 100)
+	v.buzzer.Beep(n, interval)
+}
+
+func (v *vmonitor) detectConnecting() {
+	for {
+		time.Sleep(10 * time.Second)
+		n, err := getConCount()
+		if err != nil {
+			log.Printf("failed to get users count, err: %v", err)
+			continue
+		}
+		v.chAlert <- n
+	}
+}
+
+func (v *vmonitor) alert() {
+	var currentUsers int
+	for {
+		select {
+		case n := <-v.chAlert:
+			if n > currentUsers {
+				// there are new connections, give an alert
+				go v.beep(2, 100)
+			}
+			currentUsers = n
+		default:
+			// do nothing
+		}
+		if currentUsers > 0 {
+			v.led.Blink(1, 1000)
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// getConCount is get the count of connecting to the server
+func getConCount() (int, error) {
+	cmd := `netstat -nat|grep -i "127.0.0.1:8081"|wc -l`
+	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	var count int
+	str := string(out)
+	if n, err := fmt.Sscanf(str, "%d\n", &count); n != 1 || err != nil {
+		return 0, fmt.Errorf("failed to parse the output of netstat")
+	}
+	return count, nil
 }
