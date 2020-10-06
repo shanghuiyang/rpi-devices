791007eb969a4ddec02119c9f7122cd875d80c90
Shanghui Yang <shanghui.yang@grabtaxi.com>
2020-10-06 11:54:07 +0800

    improve gps example

--- example/gps/gps.go
+++ example/gps/gps.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/shanghuiyang/rpi-devices/dev"
 )
 
 func main() {
 	g := dev.NewGPS()
-	pt, err := g.Loc()
-	if err != nil {
-		log.Printf("failed, error: %v", err)
-		return
+	defer g.Close()
+
+	for {
+		time.Sleep(1 * time.Second)
+		pt, err := g.Loc()
+		if err != nil {
+			log.Printf("failed, error: %v", err)
+			continue
+		}
+		log.Printf("%v", pt)
 	}
-	log.Printf("%v", pt)
-	g.Close()
 }
