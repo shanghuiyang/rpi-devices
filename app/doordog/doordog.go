452a9d0140d071f69eae7863347fb982eaca7887
Shanghui Yang <shanghui.yang@grabtaxi.com>
2020-12-13 00:10:31 +0800

    support face recognition for doordog

--- app/doordog/doordog.go
+++ app/doordog/doordog.go
@@ -6,11 +6,13 @@ When somebody entries your room, you will be alerted by a beeping buzzer and a b
 package main
 
 import (
-	"bytes"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 
+	"github.com/shanghuiyang/face-recognizer/face"
+	"github.com/shanghuiyang/go-speech/oauth"
 	"github.com/shanghuiyang/rpi-devices/base"
 	"github.com/shanghuiyang/rpi-devices/dev"
 	"github.com/stianeikeland/go-rpio"
@@ -30,7 +32,22 @@ const (
 	// the time of keeping alert in second
 	alertTime = 60
 	// the distance of triggering alert in cm
-	alertDist = 100
+	alertDist = 80
+
+	groupID = "mygroup"
+
+	baiduFaceRecognitionAppKey    = "your_face_app_key"
+	baiduFaceRecognitionSecretKey = "your_face_secret_key"
+)
+
+var (
+	allowlist = []string{
+		"p1",
+		"p2",
+		"p3",
+		"p4",
+		"p5",
+	}
 )
 
 func main() {
@@ -40,6 +57,7 @@ func main() {
 	}
 	defer rpio.Close()
 
+	cam := dev.NewCamera()
 	bzr := dev.NewBuzzer(pinBzr)
 	led := dev.NewLed(pinLed)
 	btn := dev.NewButton(pinBtn)
@@ -49,7 +67,10 @@ func main() {
 		return
 	}
 
-	dog := newDoordog(dist, bzr, led, btn)
+	auth := oauth.New(baiduFaceRecognitionAppKey, baiduFaceRecognitionSecretKey, oauth.NewCacheMan())
+	f := face.New(auth)
+
+	dog := newDoordog(cam, dist, bzr, led, btn, f)
 	base.WaitQuit(func() {
 		dog.stop()
 		rpio.Close()
@@ -58,20 +79,24 @@ func main() {
 }
 
 type doordog struct {
+	cam      *dev.Camera
 	dist     *dev.HCSR04
 	buzzer   *dev.Buzzer
 	led      *dev.Led
 	button   *dev.Button
+	face     *face.Face
 	alerting bool
 	chAlert  chan bool
 }
 
-func newDoordog(dist *dev.HCSR04, buzzer *dev.Buzzer, led *dev.Led, btn *dev.Button) *doordog {
+func newDoordog(cam *dev.Camera, dist *dev.HCSR04, buzzer *dev.Buzzer, led *dev.Led, btn *dev.Button, face *face.Face) *doordog {
 	return &doordog{
+		cam:      cam,
 		dist:     dist,
 		buzzer:   buzzer,
 		led:      led,
 		button:   btn,
+		face:     face,
 		alerting: false,
 		chAlert:  make(chan bool, 4),
 	}
@@ -89,7 +114,8 @@ func (d *doordog) detect() {
 	// need to warm-up the ultrasonic distance meter first
 	d.dist.Dist()
 	time.Sleep(500 * time.Millisecond)
-	var t time.Duration
+
+	t := 300 * time.Millisecond
 	for {
 		time.Sleep(t)
 		dist := d.dist.Dist()
@@ -97,14 +123,21 @@ func (d *doordog) detect() {
 			log.Printf("[doordog]bad data from distant meter, distance = %.2fcm", dist)
 			continue
 		}
-		detected := (dist < alertDist)
-		d.chAlert <- detected
 
-		t = 300 * time.Millisecond
+		detected := (dist < alertDist)
 		if detected {
 			log.Printf("[doordog]detected objects, distance = %.2fcm", dist)
-			// make a dalay detecting
-			t = 3 * time.Second
+			who, err := d.RecoginzeFace()
+			if err != nil {
+				continue
+			}
+
+			log.Printf("[doordog]it is %v", who)
+			if allowed(who) {
+				continue
+			}
+
+			d.chAlert <- detected
 			continue
 		}
 	}
@@ -137,6 +170,35 @@ func (d *doordog) alert() {
 	}
 }
 
+func (d *doordog) RecoginzeFace() (name string, err error) {
+	imgf, e := d.cam.TakePhoto()
+	if e != nil {
+		log.Printf("[doordog]failed to take phote, error: %v", e)
+		name, err = "unknow", e
+		return
+	}
+
+	users, e := d.face.Recognize(imgf, groupID)
+	if e != nil {
+		log.Printf("[doordog]failed to recognize the image, error: %v", e)
+		name, err = "unknow", e
+		return
+	}
+
+	if len(users) == 0 {
+		name, err = "unknow", nil
+		return
+	}
+
+	log.Printf("who: %v", *(users[0]))
+	if users[0].Score > 50 {
+		return users[0].UserID, nil
+	}
+
+	name, err = "unknow", nil
+	return
+}
+
 func (d *doordog) stopAlert() {
 	for {
 		pressed := d.button.Pressed()
@@ -154,22 +216,24 @@ func (d *doordog) stopAlert() {
 }
 
 func ifttt() {
-	req, err := http.NewRequest("POST", ifttAPI, bytes.NewBuffer([]byte{}))
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{
-		Timeout: 15 * time.Second,
-	}
-	resp, err := client.Do(req)
+	resp, err := http.PostForm(ifttAPI, url.Values{})
 	if err != nil {
 		log.Printf("failed to request to ifttt, error: %v", err)
 		return
 	}
 	defer resp.Body.Close()
-	return
 }
 
 func (d *doordog) stop() {
 	d.buzzer.Off()
 	d.led.Off()
+}
+
+func allowed(user string) bool {
+	for _, u := range allowlist {
+		if u == user {
+			return true
+		}
+	}
+	return false
 }
